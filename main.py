887ad186046d884a74b2f098ae4beca537a0f243
Nicholas Molina <nicholasmolina77@gmail.com>
2025-03-25 13:00:25 -0400

    implemented password checker

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-
+import re
 
 # TODO: change this to your academic email
 AUTHOR = "ngmolina@seas.upenn.edu"
@@ -13,17 +13,37 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
-    return f"Hello from my Password Validator! &mdash; <tt>{AUTHOR}</tt>"
-
-
-# This is a sample "password validator" endpoint
-# It is not yet implemented, and will return HTTP 501 in all situations
+    return f"Hello from my Password Validator! Please test your passwords! &mdash; <tt>{AUTHOR}</tt>"
 
 
 @app.route("/v1/checkPassword", methods=["POST"])
 def check_password():
     data = flask.request.get_json() or {}
     pw = data.get("password", "")
-
-    # FIXME: to be implemented
-    return flask.jsonify({"valid": False, "reason": "Not implemented"}), 501
+    
+    # Initialize reason as empty string (for valid passwords)
+    reason = ""
+    valid = True
+    
+    # Check length >= 8
+    if len(pw) < 8:
+        reason = "Password must be at least 8 characters long"
+        valid = False
+    
+    # Check for at least 2 uppercase letters
+    elif len(re.findall(r'[A-Z]', pw)) < 2:
+        reason = "Password must contain at least 2 uppercase letters"
+        valid = False
+    
+    # Check for at least 2 digits
+    elif len(re.findall(r'\d', pw)) < 2:
+        reason = "Password must contain at least 2 digits"
+        valid = False
+    
+    # Check for at least 1 special character from !@#$%^&*
+    elif len(re.findall(r'[!@#$%^&*]', pw)) < 1:
+        reason = "Password must contain at least 1 special character (!@#$%^&*)"
+        valid = False
+    
+    # Return the result with HTTP 200 status code
+    return flask.jsonify({"valid": valid, "reason": reason}), 200
